1948a3700dc7b696d00a9c3eeb1ad9234d88a8b0
hhlh27 <hannahloh27@gmail.com>
2025-01-22 21:04:45 +0800

    Add add and list functions

--- src/main/java/DazAI.java
+++ src/main/java/DazAI.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DazAI {
     public static void main(String[] args) {
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new java.util.Scanner(System.in);
         String logo = "  ____                    ___    _____ \n"
                 + " |  _ \\  __ _  _____     / _\\   |__  _|\n"
                 + " | | | |/ _` | |_  |    / /  \\    | | \n"
                 + " | |_| | (_| |  / /_   /  --- \\   | | \n"
                 + " |____/ \\__,_| |__,_| //       \\\\|____|  \n";
 
-        System.out.println("Hello from\n" + logo);
+        
         // Print greeting message
         System.out.println("____________________________________________________________");
         System.out.println(" Hello from\n" + logo);
@@ -18,12 +22,34 @@ public class DazAI {
             String input = scanner.nextLine().trim();
             if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                // Display the list of tasks
+                System.out.println("____________________________________________________________");
+                if (tasks.isEmpty()) {
+                    System.out.println(" Your task list is empty.");
+                } else {
+                    System.out.println(" These are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks.get(i));
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                // Add the input to the list of tasks
+                tasks.add(input);
+                System.out.println("____________________________________________________________");
+                System.out.println(" added: " + input);
+                System.out.println("____________________________________________________________");
             }
+
+/**
             // Echo the user's command
             System.out.println("____________________________________________________________");
             System.out.println(" " + input);
             System.out.println("____________________________________________________________");
+ **/
         }
+
         //Print exit message
         System.out.println("____________________________________________________________");
         System.out.println(" Bye. Hope to see you again soon!");
