ccde39be415708605ca78543eee2bd713b8786d8
hhlh27 <hannahloh27@gmail.com>
2025-01-22 20:34:23 +0800

    Add greeting and exit messages

---
+++ src/main/java/DazAI.java
@@ -0,0 +1,21 @@
+public class DazAI {
+    public static void main(String[] args) {
+        String logo = "  ____                    ___    _____ \n"
+                + " |  _ \\  __ _  _____     / _\\   |__  _|\n"
+                + " | | | |/ _` | |_  |    / /  \\    | | \n"
+                + " | |_| | (_| |  / /_   /  --- \\   | | \n"
+                + " |____/ \\__,_| |__,_| //       \\\\|____|  \n";
+
+        System.out.println("Hello from\n" + logo);
+        // Print greeting message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello from\n" + logo);
+        System.out.println(" Hello! I'm DazAI");
+        System.out.println(" What can I do for you?");
+        //Print exit message
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
+
