84272feea33aa6a99b9ff984939658194c3fbaf9
hhlh27 <hannahloh27@gmail.com>
2025-01-29 14:29:57 +0800

    Modify Event class to handle start and end times

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,28 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    private LocalDateTime from;
+    private LocalDateTime to;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = LocalDateTime.parse(from, INPUT_FORMAT);
+        this.to = LocalDateTime.parse(to, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
+        return "[E]" + super.toString() + " (from: " + from.format(OUTPUT_FORMAT) + " to: " + to.format(OUTPUT_FORMAT) + ")";
+    }
+
+    public String getFrom() {
+        return from.format(INPUT_FORMAT);
+    }
+
+    public String getTo() {
+        return to.format(INPUT_FORMAT);
     }
 }
