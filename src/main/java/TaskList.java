a055d17c64f267cb79064afa5c64796194d28ede
hhlh27 <hannahloh27@gmail.com>
2025-01-29 20:49:44 +0800

    Add TaskList.java class

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -3,23 +3,32 @@ import java.util.ArrayList;
 public class TaskList {
     private ArrayList<Task> tasks;
 
-    public TaskList(ArrayList<Task> tasks) {
-        this.tasks = tasks;
+    public TaskList() {
+        tasks = new ArrayList<>();
     }
 
     public void addTask(Task task) {
         tasks.add(task);
     }
 
-    public void deleteTask(int index) {
-        tasks.remove(index);
+    public Task deleteTask(int index) {
+        return tasks.remove(index);
     }
 
     public Task getTask(int index) {
         return tasks.get(index);
     }
 
+    public ArrayList<Task> getAllTasks() {
+        return tasks;
+    }
+
     public int size() {
         return tasks.size();
     }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
 }
