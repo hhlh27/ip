1df813e09b3c263980db9174eac2e72394ff45b7
hhlh27 <hannahloh27@gmail.com>
2025-01-29 14:26:18 +0800

    Modify Deadline class to use LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,23 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    private LocalDateTime by;
+    private static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+    private static final DateTimeFormatter OUTPUT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mm a");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, INPUT_FORMAT);
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(OUTPUT_FORMAT) + ")";
     }
     // Getter for description
     public String getBy() {
-        return by;
+        return by.format(INPUT_FORMAT); //
     }
 }
 
